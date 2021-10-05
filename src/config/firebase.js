97d68643c325b2268cd17df5aaeb771147e0269c
Esteben Alexis Arce Gomez <dragonforce66448@gmail.com>
2021-10-04 20:24:46 -0500

    * Add plans Feature

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, doc, query, collection, where, getDocs, getDoc, addDoc, onSnapshot } from "firebase/firestore";
 import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,5 +20,5 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
-export { auth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut };
+export { auth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, doc, query, collection, where, getDocs, getDoc, addDoc, onSnapshot };
 export default db;
